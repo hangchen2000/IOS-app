f228b48c8e620b3c70f67d0b75afadce3198bb1a
Hang Chen <hchen.web@gmail.com>
2017-10-11 23:28:49 -0700

    new push cal

--- my first IOS/ViewController.swift
+++ my first IOS/ViewController.swift
@@ -14,18 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     
-    var tapCount = 0
+  
     
     @IBAction func buttonTapped(_ sender: Any) {
         
         print(text1.text!)
         print(text2.text!)
         
-        tapCount = tapCount + 1
         
-        if tapCount > 20 {
-            theLabel.text = "tapped over " + String (tapCount)
-            print("Button Tapped over 10" )        }
+       
+            theLabel.text = "new " + String (Double(text1.text!)! + Double(text2.text!)!)
+            print("Button Tapped" )
     }
     
     override func viewDidLoad() {
